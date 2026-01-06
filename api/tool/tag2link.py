c70033f057f05702d154ed51a63e4331277db352
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2026-01-06 01:38:11 +0100

    Use tag2link oder to get prefered link #562 #531

--- api/tool/tag2link.py
+++ api/tool/tag2link.py
@@ -7,15 +7,17 @@ from typing import Dict, List
 class tag2link:
     def __init__(self, rulesFiles: str):
         rules_json = json.load(open(rulesFiles, "r", encoding="utf-8"))
-        self.rules: Dict[str, str] = dict(
-            map(
-                lambda rule: [
-                    rule["key"][4:],
-                    rule["url"],
-                ],
-                filter(lambda rule: rule["key"].startswith("Key:"), rules_json),
-            )
-        )
+        self.rules: Dict[(str, str), str] = {}
+        for rule in rules_json:
+            key = rule["key"]
+            if not key.startswith("Key:"):
+                continue
+            key = key[4:]
+
+            if key in self.rules:
+                continue
+
+            self.rules[key] = rule["url"]
 
     def addLinks(self, tags: Dict[str, str]) -> List[Dict[str, str]]:
         links = []
@@ -27,7 +29,13 @@ class tag2link:
 
 
 if __name__ == "__main__":
-    t2l = tag2link("tag2link_sources.json")
+    t2l = tag2link("api/tool/tag2link_sources.json")
+
+    print(t2l.addLinks({"website": "a"}))
+    assert(t2l.addLinks({"website": "a"}) == [{"k": "website", "v": "a", 'vlink': 'a'}])
+    print(t2l.addLinks({"wikimedia_commons": "a"}))
+    assert(t2l.addLinks({"wikimedia_commons": "a"}) == [{"k": "wikimedia_commons", "v": "a", "vlink": "https://commons.wikimedia.org/wiki/a"}])
+
     print(t2l.addLinks({"oneway": "yes"}))
     print(t2l.addLinks({"url": "plop.com"}))
     print(t2l.addLinks({"url": "http://plop.com"}))
